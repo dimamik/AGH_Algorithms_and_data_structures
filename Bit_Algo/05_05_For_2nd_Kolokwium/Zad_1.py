3b6b8f62195003eb889deba5f66273af61515ee2
dimamik <57190429+dimamik@users.noreply.github.com>
2020-05-06 11:45:57 +0200

    main

---
+++ Bit_Algo/05_05_For_2nd_Kolokwium/Zad_1.py
@@ -0,0 +1,7 @@
+""" 
+a jakbym zrobic od drugiej strony, ze tworzymy ten graf, dodajac krawedzie o wag najwyzszych do najnizyszch i jak dodatkowa krawedz nie łaczy czerwonych to ok, a jak łaczy to nie dodajemy jej i idziemy dalej
+
+Bierzemy krawedzi i sortujemy, dalej patrzymy od rosnacego do minimum (find union)
+
+
+ """
---
+++ Bit_Algo/05_05_For_2nd_Kolokwium/Zad_2.py
@@ -0,0 +1,6 @@
+""" 
+от польз. Wojciech Ciężobka польз. все:
+To zadanie 2, też bym robił przez find union, tworzyl zbiory gdzie wszyttkie x sa rowne
+от польз. Paweł Kruczkiewicz польз. все:
+Może każdą równosc dodajemy do zbioru, a potem dla kazdej nierownosci sprawdzamy czy sa w jednym zbiorze. Jesli sa, to mamy sprzecznosc
+ """
