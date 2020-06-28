17e8b3ef7ce7434a5e3f013009f1540d9fe28346
dimamik <57190429+dimamik@users.noreply.github.com>
2020-06-28 17:31:59 +0200

    Bug_with_low_fixed

--- FOR EXAM/Zad_Zastosowania_DFS/Mosty.py
+++ FOR EXAM/Zad_Zastosowania_DFS/Mosty.py
@@ -40,18 +40,21 @@ def DFS_Usual_Visit_Parent_and_Wsteczne(u_index,tab_of_v):
     time+=1
     tab_of_v[u_index].visited=True
     tab_of_v[u_index].entry=time
-    """ Żeby nie nadpisywać już zapisanych """
-    tab_of_v[u_index].low=min(time,tab_of_v[u_index].low)
+    """ Default wartosc low """
+    tab_of_v[u_index].low=time
     for v_ind in tab_of_v[u_index].tab_index_incident:
         if not tab_of_v[v_ind].visited:
             tab_of_v[v_ind].parrent=u_index
-            DFS_Usual_Visit_Parent_and_Wsteczne(v_ind,tab_of_v)    
+            DFS_Usual_Visit_Parent_and_Wsteczne(v_ind,tab_of_v)
+            """ Podnosze wartosc w gore drzewa DFS """
+            tab_of_v[u_index].low=min(tab_of_v[v_ind].low,tab_of_v[u_index].low)    
         else:
             if tab_of_v[u_index].parrent!=v_ind:
-                """ Krawędź wsteczna tu jest, bo już odwiedzony i nie rodzic"""
-                tab_of_v[u_index].low=min(tab_of_v[v_ind].low,tab_of_v[u_index].entry)
+                """ Krawędź wsteczna tu jest, bo już odwiedzony i nie rodzic,
+                wiec uaktualizujemy low"""
+                """ !!!UWAGA PORÓWNUJE Z ENTRY WSTECZNYM A NIE LOW!!! """
+                tab_of_v[u_index].low=min(tab_of_v[u_index].low,tab_of_v[v_ind].entry)
                 tab_of_v[u_index].wsteczna=v_ind
-
     time+=1
     tab_of_v[u_index].process=time
 
@@ -59,7 +62,7 @@ def DFS_Parent_and_Wsteczne(tab_of_v):
     for v_ind in range(len(tab_of_v)):
         if not tab_of_v[v_ind].visited:
             DFS_Usual_Visit_Parent_and_Wsteczne(v_ind,tab_of_v)
-    """ Writing out the parrents tab """
+    """ Dużo tablic nie koniecznie potrzebnych"""
     tab_p=[0]*len(tab_of_v)
     tab_w=[0]*len(tab_of_v)
     tab_l=[0]*len(tab_of_v)
@@ -71,23 +74,21 @@ def DFS_Parent_and_Wsteczne(tab_of_v):
         tab_time[i]=tab_of_v[i].entry
     return tab_time,tab_p,tab_w,tab_l
 
-def MostyDFS(tab_of_v):
+def Mosty_DFS(tab_of_v):
     """ Last two returns are useless in this case """
     tab_entry,tab_p,tab_w,tab_l=DFS_Parent_and_Wsteczne(tab_of_v)
-    for kr_wst in tab_w:
-        if kr_wst!=-1 and tab_p[kr_wst]!=-1:
-            while tab_p[kr_wst]!=-1:
-                tab_of_v[tab_p[kr_wst]].low=min(tab_of_v[tab_p[kr_wst]].low,
-                tab_of_v[kr_wst].low)
-                kr_wst=tab_p[kr_wst]
+
     tab_of_mosts=[]
     for i in range(len(tab_of_v)):
         if tab_p[i]!=-1 and tab_of_v[i].low==tab_of_v[i].entry:
             tab_of_mosts.append((tab_p[i],i))
     """ return tab_of_mosts """
+    for i in range(len(tab_of_v)):
+        tab_l[i]=tab_of_v[i].low
     if tab_of_mosts==[]:
         return "No Mosts Found"
     return tab_of_mosts
 
-""" tab_v=BuildFromInc([[1,3],[0,2],[1,3,4],[0,2,7],[2,5,6],[4,6],[4,5],[3]])
-print(MostyDFS(tab_v)) """
+tab_v=BuildFromInc([[1,3],[0,2],[1,3,4],[0,2,7],[2,5,6],[4,6],[4,5],[3]])
+tab_v=BuildFromInc([[1,2],[0,2],[1,0,3,4],[2,4],[2,3]])
+print(Mosty_DFS(tab_v))
