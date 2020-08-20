77fe7282e07c4773a1483bf3d7fe453228b6c5f7
dimamik <dymitrmikielewicz@gmail.com>
2020-08-20 18:18:19 +0200

    Reorganization_2.0

--- Standart_Functions_Examples.py
+++ Standart_Functions_Examples.py
@@ -1,3 +1,21 @@
+class Vertex():
+    def __init__(self,value1,value2):
+        self.value1=value1
+        self.value2=value2
+    """ 
+    Nadpisuje porownanie
+     """
+    def __lt__(self, value):
+        return (self.value2<value.value2)
+
+v = Vertex(5,2)
+u = Vertex(1,3)
+print(v>u)
+
+
+
+
+
 from queue import *
 """ Kolejka FIFO """
 Q=Queue()
